4593c9a76dfd001950464381d18e25d562bc3e8b
Bruno Jullien <bruno.jullien@gmail.com>
2023-01-28 14:39:14 -0800

    55: TextViews

--- app/textviews/src/main/java/com/bbj/textviews/MainActivity.kt
+++ app/textviews/src/main/java/com/bbj/textviews/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.bbj.textviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * In Kotlin a class variable must be initialized or be abstract
+     * but sometime we just want to initialize later; 2 options:
+     *      1. lateinit keyword: code editor is happier (most common for components)
+     *      2. var myText : TextView? = null
+     */
+    lateinit var myText : TextView
+    /**
+        onCreate() is called when activity is created
+        - it is the very first lifecycle function called
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * R: represents the resources folder
+         */
+        myText = findViewById(R.id.textExample)
+        myText.setTextColor(Color.RED)
+
+        myText.setText("This is an example")
+        //same as myText.text = "This is an example"
+
+        myText.setOnClickListener {
+            myText.setTextColor(Color.WHITE)
+            myText.setBackgroundColor(Color.BLACK)
+        }
+
     }
 }
